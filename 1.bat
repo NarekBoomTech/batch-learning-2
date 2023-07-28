9c6ea75c7e850f7257da30d3435c0915afeaa93a
FIRST_NAME LAST_NAME <nareksargsyan950@gmail.com>
2023-07-28 21:03:21 +0400

    c1

--- 1.bat
+++ 1.bat
@@ -2,5 +2,5 @@ git config --add --bool push.autoSetupRemote true
 git add .
 git commit -m "c1"
 git push
-gh pr create --base master -t "c1" -b ""
+gh pr create --base "master" -t "c1" -b ""
 gh pr merge -m
